c9b8de3ec4392074ddc7e5550cc1dd3724435f60
SadkiratSingh <sadkiratsingh@yahoo.com>
2020-05-29 13:50:21 +0530

    navbar added sm-lg

--- static/js/my_project_script.js
+++ static/js/my_project_script.js
@@ -1,11 +1,30 @@
 const getSemDetailsButtons=document.querySelectorAll('.sem-subject-details');
 const formCont=document.getElementsByClassName('form-container')[0];
 const chooseCourse=document.querySelector('.choosecourse');
+const closeFormButton=document.querySelector('.close-form');
+const openNavBar=document.querySelector('.nav-icon');
+const navList=document.querySelector('.nav-list')
 var subjectsList=undefined;
 
+//button clicks action//
+closeFormButton.onclick=closeForm;
+openNavBar.onclick=navbardisplay;
 getSemDetailsButtons.forEach((button)=>{
     button.onclick=loadSemCourses;
 })
+//button clicks action//
+
+//header//
+function navbardisplay(){
+    navList.classList.toggle('nav-open')
+}
+//header
+
+//for getNotesForm
+
+function closeForm(){
+    formCont.style.display='none';
+}
 
 function loadSemCourses(){
     formCont.style.display='block';
@@ -31,3 +50,5 @@ function createFormContent(){
     }
     chooseCourse.innerHTML=formstr;
 }
+
+//for getNotesForm
